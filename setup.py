f550fa30822248d39b630ab5b302fc01b6112b1a
Tyler Bruno <31492155+tybruno@users.noreply.github.com>
2025-02-21 15:36:50 -0700

    Fixing small typing issues and validation in initializer. (#8)
    * Fixing small typing issues and validation in initializer.
    
    * running blue.
    
    * fixing typing.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import (
     setup,
 )
 
-__version__ = 'v1.2'
+__version__ = 'v1.3'
 __author__ = 'Tyler Bruno'
 DESCRIPTION = 'Raising Exceptions From the Dead by Re-raising Them With New Exception \
     Types'
