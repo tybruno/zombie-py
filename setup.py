b5cf18376cffd30f0ab64bb1faf5013037388ad6
tybruno <ty@brunoparadigm.com>
2025-02-21 13:24:32 -0700

    Reverting version to python 3.10

--- setup.py
+++ setup.py
@@ -24,8 +24,7 @@ setup(
     keywords='python re-raise exceptions',
     url='https://github.com/tybruno/zombie-py',
     license='MIT',
-    package_data={'zombie-py': ['py.typed'], '': ['images/zombie-py.png']},
-    include_package_data=True,
+    package_data={'zombie-py': ['py.typed']},
     packages=find_packages(),
     install_requires=INSTALL_REQUIRES,
     classifiers=[
